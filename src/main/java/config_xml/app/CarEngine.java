8bde5dcf2aeadc1efd151e15e79c5510ca1c3856
mskomarov <mx123@yandex.ru>
2019-10-19 23:58:43 +1000

    Adding @Scope("prototype") to class CarEngine.

--- src/main/java/config_xml/app/CarEngine.java
+++ src/main/java/config_xml/app/CarEngine.java
@@ -1,8 +1,10 @@
 package config_xml.app;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("carEngine")
+@Scope("prototype")
 public class CarEngine {
 
     public void doStart() {
